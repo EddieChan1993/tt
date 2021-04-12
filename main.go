9899d46025940d59316fa5828216d1b9c9cad1eb
段成文 <duanchengwen@dianchu.com>
2021-04-12 11:14:22 +0800

    增加当前时间戳显示

--- main.go
+++ main.go
@@ -29,20 +29,44 @@ func main() {
 
 	gridElems := TimestampToDate()
 	gridElems2 := DateToTimestamp()
+	timerGridElems3, ticker := TimeNow()
+	defer ticker.Stop()
 	gridElems = append(gridElems, gridElems2...)
+	gridElems = append(gridElems, timerGridElems3...)
 	grid := container.New(layout.NewGridLayout(3), gridElems...)
 	myWindow.SetContent(grid)
 	myWindow.Resize(fyne.NewSize(500, 50))
 	myWindow.ShowAndRun()
 }
 
+func TimeNow() ([]fyne.CanvasObject, *time.Ticker) {
+	now := time.Now().UnixNano() / 1e6
+	millionSec := strconv.Itoa(int(now))
+	timeStampInp := widget.NewLabel(millionSec)
+	timeStampInp.SetText(strconv.Itoa(int(now)))
+	click1 := widget.NewButton("COPY", func() {
+		//复制到剪切版
+		copyClipBoard(timeStampInp.Text)
+	})
+	ticker := time.NewTicker(time.Second)
+	go func(label *widget.Label) {
+		for t := range ticker.C {
+			millionSec := strconv.Itoa(int(t.UnixNano() / 1e6))
+			label.SetText(millionSec)
+		}
+	}(timeStampInp)
+	return []fyne.CanvasObject{
+		timeStampInp, click1,
+	}, ticker
+}
+
 func TimestampToDate() []fyne.CanvasObject {
 	timeStampInp := widget.NewEntry()
 	now := time.Now().UnixNano() / 1e6
 	timeStampInp.SetText(strconv.Itoa(int(now)))
-	timeStampInp.SetPlaceHolder("time stamp")
-	text3 := widget.NewLabel("date")
-	click1 := widget.NewButton("click", func() {
+	timeStampInp.SetPlaceHolder("TIME STAMP")
+	text3 := widget.NewLabel("DATE")
+	click1 := widget.NewButton("CLICK", func() {
 		nums, err := strconv.Atoi(timeStampInp.Text)
 		if err != nil {
 			fmt.Println(err)
@@ -54,8 +78,7 @@ func TimestampToDate() []fyne.CanvasObject {
 		}
 		text3.SetText(date)
 		//复制到剪切版
-		clipboard := fyne.CurrentApp().Driver().AllWindows()[0].Clipboard()
-		clipboard.SetContent(date)
+		copyClipBoard(date)
 		fmt.Printf("%d--->%s\n", nums, date)
 	})
 	return []fyne.CanvasObject{
@@ -68,9 +91,10 @@ func DateToTimestamp() []fyne.CanvasObject {
 	now := time.Now().UnixNano() / 1e6
 	date, _ := timeStampToDate(int(now))
 	timeStampInp.SetText(date)
-	timeStampInp.SetPlaceHolder("date")
-	text3 := widget.NewLabel("time stamp")
-	click1 := widget.NewButton("click", func() {
+	timeStampInp.SetPlaceHolder("DATE")
+	text3 := widget.NewLabel("TIME STAMP")
+
+	click1 := widget.NewButton("CLICK", func() {
 		stamp, err := time.ParseInLocation(timeTemplate1, timeStampInp.Text, time.Local)
 		if err != nil {
 			fmt.Println(err)
@@ -78,8 +102,7 @@ func DateToTimestamp() []fyne.CanvasObject {
 		}
 		millionSec := strconv.Itoa(int(stamp.UnixNano() / 1e6))
 		text3.SetText(millionSec)
-		clipboard := fyne.CurrentApp().Driver().AllWindows()[0].Clipboard()
-		clipboard.SetContent(millionSec)
+		copyClipBoard(millionSec)
 		fmt.Printf("%s--->%s\n", stamp, millionSec)
 	})
 	return []fyne.CanvasObject{
@@ -94,4 +117,10 @@ func timeStampToDate(t int) (date string, err error) {
 	}
 	date = time.Unix(int64(nums/1000), 0).Format(timeTemplate1)
 	return
+}
+
+func copyClipBoard(context string) {
+	clipboard := fyne.CurrentApp().Driver().AllWindows()[0].Clipboard()
+	clipboard.SetContent(context)
+	fmt.Println("success clipboard", context)
 }
